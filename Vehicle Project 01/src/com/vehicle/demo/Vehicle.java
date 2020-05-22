81b66330bffceb6028dcb977e2063a07e89d2a6c
vinithakadudhula <65206828+vinithakadudhula@users.noreply.github.com>
2020-05-22 17:46:07 +0530

    Add files via upload
    By vinitha kadudhula

---
+++ Vehicle Project 01/src/com/vehicle/demo/Vehicle.java
@@ -0,0 +1,25 @@
+package com.vehicle.demo;
+
+public abstract class Vehicle {
+    private String Name;
+    private String  Model;
+	private int Price;
+	public Vehicle(String name, String model, int price) {
+		super();
+		Name = name;
+		Model = model;
+		Price = price;
+	}
+	void getDetails(){
+		System.out.println("Name "+Name);
+		System.out.println("Model "+Model);
+		System.out.println("Price "+Price);
+	}
+		
+		abstract String [] showVehicleTypes();
+		abstract  int getMileage();
+		abstract String getAccesories();
+		 abstract String getPrintSafety();
+		 
+			
+}
